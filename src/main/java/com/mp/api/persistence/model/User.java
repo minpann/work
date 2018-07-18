6ebbbba3deb6a4b709a87dceba08e09aeb8ad836
minpann <mpbeer@163.com>
2018-07-18 13:01:45 +0800

    modify profile

--- src/main/java/com/mp/api/persistence/model/User.java
+++ src/main/java/com/mp/api/persistence/model/User.java
@@ -1,13 +1,17 @@
 package com.mp.api.persistence.model;
 
+import java.util.Date;
+
 public class User {
 	private int id;
-	
+
 	private String username;
-	
+
 	private String address;
-	
+
 	private String description;
+
+	private Date date;
 
 	public int getId() {
 		return id;
@@ -41,9 +45,22 @@ public class User {
 		this.description = description;
 	}
 
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", username=" + username + ", address="
-				+ address + ", description=" + description + "]";
+		return "User{" +
+				"id=" + id +
+				", username='" + username + '\'' +
+				", address='" + address + '\'' +
+				", description='" + description + '\'' +
+				", date=" + date +
+				'}';
 	}
 }
